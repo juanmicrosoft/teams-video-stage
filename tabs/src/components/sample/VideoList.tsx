2362254714f8cfb78e9f94b4b31839488a255f1a
Juan Rivera <juanrivera@microsoft.com>
2021-07-26 17:32:31 -0400

    Initial cut

---
+++ tabs/src/components/sample/VideoList.tsx
@@ -0,0 +1,36 @@
+import { List, Layout, Image, Flex, Text, Divider } from "@fluentui/react-northstar";
+import { AddVideo } from "./AddVideo";
+
+export function VideoList() {
+
+    const ellipsis = <span>&hellip;</span>
+
+    const items = [
+        { key: '1', content: 'https://www.youtube.com/watch?v=usqvLtEq2qQ', header: 'My Awkward Audition for The Office',  endMedia: ellipsis, media: (
+            <Image styles={{maxWidth:'70px', padding:'5px'}} src="http://i3.ytimg.com/vi/usqvLtEq2qQ/hqdefault.jpg"/>
+        )},
+        { key: '2', content: 'https://www.youtube.com/watch?v=VDDPoYOQYfM', header: 'Microsoft Teams Tutorial in 10 min',  endMedia: ellipsis, media: (
+            <Image styles={{maxWidth:'70px', padding:'5px'}} src="http://i3.ytimg.com/vi/VDDPoYOQYfM/hqdefault.jpg"/>
+        )},
+        { key: '3', content: 'https://www.youtube.com/watch?v=jugBQqE_2sM', header: 'Welcome to Microsoft Teams',  endMedia: ellipsis, media: (
+            <Image styles={{maxWidth:'70px', padding:'5px'}} src="http://i3.ytimg.com/vi/jugBQqE_2sM/hqdefault.jpg"/>
+        )}
+    ];
+
+    return (
+        <Layout styles={{padding:'20px'}}>
+            <Flex column>
+                <Text content="Video List" weight="bold" size="large" />
+                <Text content="List of videos you can share during the meeting" size="small" />
+            </Flex>
+            <Divider size={1} color="brand" ></Divider>
+            <List selectable items={items} />
+            <Layout styles={{margin:'20px'}}>
+                <Flex gap="gap.smaller">
+                    <AddVideo/>
+                </Flex>
+            </Layout>
+        </Layout>
+    );
+
+}
